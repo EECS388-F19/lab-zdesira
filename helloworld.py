ac0b16eccb2c4bae5fe1b887ce61e3684d758a95
zdesira <zdesira@umich.edu>
2019-09-09 16:46:50 -0400

    Pushing python script

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+import random
+from random import randint
+first = random.randint(0, 100)
+second = random.randint(0, 100)
+print("Zachary Desira")
+print(first)
+print(second)
+x = first + second
+print ("Sum = " + str(x))
+avg = x / 2.0
+print ("Average = " + str(avg))
